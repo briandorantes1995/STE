0574f293a3b65aafaf608822f5cd78ae8c5cae5d
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-06-14 23:09:32 -0600

    se modifican y completan algunas vistas, se logra login con api

--- app/src/main/java/com/example/ste/MainActivity.java
+++ app/src/main/java/com/example/ste/MainActivity.java
@@ -4,15 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+     String data;
+
+     Boolean Validate;
+
+    OkHttpClient client = new OkHttpClient();
+     EditText username;
+     EditText password;
+    Button loginButton;
+
+
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent secondActivityIntent = new Intent(this, Home.class);
-        startActivity(secondActivityIntent);
+        username = findViewById(R.id.usuario);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.iniciarsesion);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Post(username.getText().toString(),password.getText().toString());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+
+    public void Post(String User,String Password) throws Exception {
+        RequestBody formBody = new FormBody.Builder()
+                .add("user", User)
+                .add("password", Password)
+                .build();
+        Request request = new Request.Builder()
+                .url("https://api-ste.smartte.com.mx/controller/sign_in.php")
+                .post(formBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            data = response.body().string();
+            JSONObject json = new JSONObject(data);
+            Validate = json.getBoolean("isValidUser");
+            System.out.println(Validate);
+            if (Validate) {
+                Intent secondActivityIntent = new Intent(this, Home.class);
+                startActivity(secondActivityIntent);
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/ste/PaymentFragment.java
+++ app/src/main/java/com/example/ste/PaymentFragment.java
@@ -1,9 +1,11 @@
 package com.example.ste;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,4 +17,7 @@ public class PaymentFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_payment, container, false);
     }
+
+    // Launches photo picker in single-select mode.
+// This means that the user can select one photo or video.
 }
